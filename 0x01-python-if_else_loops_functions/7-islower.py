e1792dcadb134b7a22954a798922597a4ef528e9
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-11-28 14:46:51 -0800

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(ord('a'), ord('z') + 1):
-            if c == chr(i):
-                return True
+        if c == chr(i):
+            return True
     return False

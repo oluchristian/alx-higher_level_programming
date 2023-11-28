31ab13530b1457e089eb5b0618fb8ff0db4f04f7
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-11-28 15:25:03 -0800

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        ascii_val = ord(char)
+        if 97 <= ascii_val <= 122:
+            ascii_val -= 32
+        print(chr(ascii_val), end='')
+    print()

dd3dca7358f2f4b6b6eb7f8e3e5f077843b353d3
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-11-28 20:55:57 -0800

    printtebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-        print("{}".format(chr(i)), end='')
+for i in range(26):
+    if i % 2 == 0:
+        print('{:c}'.format(122 - i), end='')
+    else:
+        print('{:c}'.format(90 - i), end='')

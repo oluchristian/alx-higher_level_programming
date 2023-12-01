e1b0da953e73be1698bf77e2ea27110c15a468ef
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-11-30 18:12:49 -0800

    formatted

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ argc = len(arguements)
 count = 1
 if __name__ == "__main__":
     if argc == 1:
-        print("{} arguments.".format(0))
+        print("{:d} arguments.".format(0))
     elif argc == 2:
-        print("{} argument.".format(argc - 1))
-        print("{}: {}".format(argc - 1, arguements[1]))
+        print("{:d} argument.".format(argc - 1))
+        print("{:d}: {:s}".format(argc - 1, arguements[1]))
     elif argc > 2:
-        print("{} arguments.".format(argc - 1))
+        print("{:d} arguments.".format(argc - 1))
         for args in arguements:
             if args != arguements[0]:
-                print("{}: {}".format(count, args))
-                count += 1 
+                print("{:d}: {:s}".format(count, args))
+                count += 1

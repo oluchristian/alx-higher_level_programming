8656025eb37c152e6e8540e181a66716fec0ef5a
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-11-30 18:06:32 -0800

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+import sys
+arguements = sys.argv
+argc = len(arguements)
+count = 1
+if __name__ == "__main__":
+    if argc == 1:
+        print("{} arguments.".format(0))
+    elif argc == 2:
+        print("{} argument.".format(argc - 1))
+        print("{}: {}".format(argc - 1, arguements[1]))
+    elif argc > 2:
+        print("{} arguments.".format(argc - 1))
+        for args in arguements:
+            if args != arguements[0]:
+                print("{}: {}".format(count, args))
+                count += 1
+    

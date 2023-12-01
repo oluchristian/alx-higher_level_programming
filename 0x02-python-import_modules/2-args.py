997e777c8d8333dfae43aa7e8f3bdbdbb936a500
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-11-30 18:43:41 -0800

    arguements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 import sys
-arguements = sys.argv
+arguements = sys.argv[1:]
 argc = len(arguements)
+string = "{:d} argument"
 count = 1
-if __name__ == "__main__":
-    if argc == 1:
-        print("{:d} arguments.".format(0))
-    elif argc == 2:
-        print("{:d} argument.".format(argc - 1))
-        print("{:d}: {:s}".format(argc - 1, arguements[1]))
-    elif argc > 2:
-        print("{:d} arguments.".format(argc - 1))
-        for args in arguements:
-            if args != arguements[0]:
-                print("{:d}: {:s}".format(count, args))
-                count += 1
+if __name__ != "__main__":
+    exit()
+if argc == 0:
+    string += 's.'
+elif argc == 1:
+    string += ':'
+elif argc > 1:
+    string += 's:'
+print(string.format(argc))
+for args in arguements:
+    print("{:d}: {:s}".format(count, args))
+    count += 1

987a96c1beb1ff9e54f230e7024c652b8ba283ef
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-12-04 17:25:53 -0800

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for arr in matrix:
+        for i in range(len(arr)):
+            print("{:d}".format(arr[i]), end='')
+            if i != len(arr) - 1:
+                print(' '.format(), end='')
+        print("".format())

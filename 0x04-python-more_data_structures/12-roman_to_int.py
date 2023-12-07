0ee8e9df93f06e04d422eff0e18d1447ede79744
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-12-07 09:58:05 -0800

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    result = 0
+    prev_value = 0
+    for key in roman_string:
+        value = roman_numerals.get(key, 0)
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+        prev_value = value
+    return result

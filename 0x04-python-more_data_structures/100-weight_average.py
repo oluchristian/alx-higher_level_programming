5cee37beb6eaacc7fb3f739046586d53ed3ae985
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-12-07 18:41:15 -0800

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,8 +4,7 @@ def weight_average(my_list=[]):
         return 0
     product_list = []  # List to store products of each tuple
     second_values = []  # List to store second elements of each tuple
-    original_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-    for tuple_item in original_list:
+    for tuple_item in my_list:
         first_element, second_element = tuple_item
         product_list.append(first_element * second_element)
         second_values.append(second_element)

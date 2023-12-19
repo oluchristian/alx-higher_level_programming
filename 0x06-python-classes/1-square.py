e538edcb120ad50cac62f3831dca4ab15398cde2
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2023-12-19 15:58:19 -0800

    class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+"""An Square class"""
+
+
+class Square():
+    """a class Square that defines a square by
+        - private instance attribute
+        - Instantiation with size
+        """
+    def __init__(self, size):
+        """Initializes a Square instance."""
+        self.__size = size

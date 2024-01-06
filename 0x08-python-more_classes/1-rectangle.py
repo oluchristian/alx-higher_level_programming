a34b5303b1b2068ba7044dd8ed56b42cc2f3a51c
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-01-06 06:13:59 -0800

    1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,13 +6,13 @@ class Rectangle:
     """a rectangle class"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-    
+        self.__width = width
+        self.__height = height
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -20,11 +20,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):

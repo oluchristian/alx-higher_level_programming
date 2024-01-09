73064f6603ea12f1f947f8f3eaa0c84e2c53d04c
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-01-08 20:24:47 -0800

    is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""instance or subclass of a specified class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true or flase if instance or subclass"""
+    return isinstance(obj, a_class)

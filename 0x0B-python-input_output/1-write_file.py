02375f058eba2b844f944ffc02b0c855da180e69
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-01-12 18:18:28 -0800

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"file input and out put. Writing to file"
+
+
+def write_file(filename="", text=""):
+    "write to a file and return the number of characters "
+    with open(filename, "w", encoding="UTF8") as file:
+        writefile = file.write(text)
+        return file.tell(writefile)

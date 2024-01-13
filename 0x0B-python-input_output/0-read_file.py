98e2ad3a1073c812db9d61d1ce91c8675ebd53b8
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-01-12 18:09:27 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"file input and output"
+
+
 def read_file(filename=""):
+    "function to read a file"
     with open(filename, "r", encoding='utf-8') as file:
         readfile = file.read()
         print(readfile, end="")

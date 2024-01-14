a55bfe3af505a018b3e2af0dbfe74548185606fe
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-01-14 04:37:48 -0800

    9-students

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,7 +8,7 @@ class Student:
     """ class Student that defines a student"""
     def __init__(self, first_name, last_name, age):
         """Initializes the student"""
-        self.first__name = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 

b71bb9a801c7e51fa400ff5b0ff9854d2b6a5dfb
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-02-13 20:20:23 -0800

    corrected task 0 and a script that inserts a new row in the table first_table

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
 -- a script that lists all databases of your MySQL server
 -- SQL query to list all databases on a server
-SHOW BATABASES;
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Insert values into table
+-- Execution: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");

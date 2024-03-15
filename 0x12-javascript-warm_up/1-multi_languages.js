2757c55bb66f9e8ba888162ee7c25e2e12a4f827
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-03-15 10:59:30 -0700

    a script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 // A script that prints 3 lines
 
-let lines = 'C is fun\nPython is cool\navaScript is amazing';
-console.log(lines);
+console.log('C is fun\nPython is cool\navaScript is amazing');

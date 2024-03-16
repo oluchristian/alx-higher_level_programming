3f83b8367057dc4f8aa4d80563586f874cb0605b
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-03-16 14:02:10 +0100

    a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//  a script that prints a square
+
+const size = process.argv[2];
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(size); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
+}

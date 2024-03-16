f935b20f1f13fa8b1ec888260d68c10eb2ddb291
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-03-15 22:56:44 -0700

    priints 3 lines using a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
+// prints 3 lines: (like 1-multi_languages.js) by using a loop
 
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 

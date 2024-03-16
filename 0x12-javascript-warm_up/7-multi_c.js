fb30105e29116edfa0d047b448c06a87c07e83bc
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-03-15 23:09:37 -0700

    a script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a script that prints x times “C is fun”
+
+const statement = 'C is fun';
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(statement);
+  }
+}

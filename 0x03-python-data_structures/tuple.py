08d36ed5ce987ae93a954efecd0cbe59f1751460
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-03-15 22:00:23 -0700

    checks if a number is an integer and prints

---
+++ 0x03-python-data_structures/tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    if len_a == 0:
+        a1 = 0
+        a2 = 0
+    elif len_a == 1:
+        a1 = 

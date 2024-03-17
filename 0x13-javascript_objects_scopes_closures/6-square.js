2d12f3a7428b4f719373546a248b8787ece81d00
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-03-17 14:22:35 +0100

    a class Square that defines a square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// a class Square that defines a square and inherits from Square of 5-square.js
+
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;

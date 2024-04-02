dfe55a0a0f2c03b4d9d7e85f50eb79e7031e5a4d
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-02 03:47:51 +0100

    python script that fetches a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""0x11. Python - Network #1"""
+import urllib.request
+if __name__ == "__main__":
+	req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+	with urllib.request.urlopen(req) as response:
+		the_page = response.read()
+		print("Body response:")
+		print("\t- type:", type(the_page))
+		print("\t- content:", the_page)
+		print("\t- utf8 content:", the_page.decode('utf-8'))

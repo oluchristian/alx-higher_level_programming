cf9b2d6baa256fd4501984f0e3b739d2bfb0acc6
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-02 05:45:25 +0100

    Python script that takes in a URL and an email address, sends a POST request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     payload = {'email': sys.argv[2]}
-    r = requests.post(url, params=payload)
+    r = requests.post(url, data=payload)
     print(r.text)

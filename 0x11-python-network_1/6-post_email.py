a4f6cd8c36518be2dd9cf8e53887d3b6eb4940cb
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-02 05:43:15 +0100

    Python script that takes in a URL and an email address, sends a POST request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email address, sends a POST request"""
+"""Python script that sends a POST request"""
 import requests
 import sys
 if __name__ == "__main__":

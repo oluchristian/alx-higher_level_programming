3642ee51c3d9483825982bfb4d5e06e699d8a393
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-02 05:38:27 +0100

    Python script that takes in a URL, sends a request to the URL

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,5 +2,6 @@
 """Python script that takes in a URL, sends a request to the URL"""
 import requests
 import sys
-r = requests.get(sys.argv[1])
-print(r.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers.get("X-Request-Id"))

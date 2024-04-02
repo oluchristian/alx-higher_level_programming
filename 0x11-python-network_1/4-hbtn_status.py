f87154e1de0f57e44b44f6b57c3c00da40db350c
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-04-02 05:34:25 +0100

    code should not be executed when imported

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import requests
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
-if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type:", type(r.text))
-    print("\t- content:", r.text)
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type:", type(r.text))
+print("\t- content:", r.text)
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,6 +2,5 @@
 """Python script that takes in a URL, sends a request to the URL"""
 import requests
 import sys
-if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    print(r.headers["X-Request-Id"])
+r = requests.get(sys.argv[1])
+print(r.headers["X-Request-Id"])
